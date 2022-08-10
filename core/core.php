c61c1f82fbec894b1b78844c56dccccaa6919010
OneGameKoTT <i.misterok21@gmail.com>
2022-08-10 22:07:45 +0300

    [HTML+Tree] dummy tree, html template

---
+++ core/core.php
@@ -0,0 +1,20 @@
+<?php
+require("config.php");
+
+
+
+$top = <<<HTML
+<!DOCTYPE >
+<html>
+    <head>
+        <title>FoxyForum</title>
+        <link rel="stylesheet" href="core/styles/main.css">
+    </head>
+    <body>
+        
+HTML;
+
+$bottom = <<<HTML
+    </body>
+</html>
+HTML;
--- index.php
+++ index.php
@@ -1,2 +1,76 @@
-<h1>Hello world!</h1>
-<h2>Git sync done.</h2>
+<?php
+require("core/core.php");
+
+echo $top;
+?>
+
+    <div class="content-wrapper">
+        <div class="content">
+            <div class="cblock">
+                <div class="bcat">
+                    <div class="title">Category one</div>
+                    <div class="bcatc">
+                        <div class="tname">
+                            <div class="bcatci">Icon</div>
+                            <div class="subd">
+                                <span>Offtop</span>
+                                <div class="tsubcat">
+                                    <a href="#" class="tsubcata">DRM</a>
+                                    <a href="#" class="tsubcata">Foxies</a>
+                                    <a href="#" class="tsubcata">Dogs</a>
+                                    <a href="#" class="tsubcata">Cats</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="bcatc">
+                        <div class="tname">
+                            <div class="bcatci">Icon</div>
+                            <div class="subd">
+                                <span>Offtop</span>
+                                <div class="tsubcat">
+                                    <a href="#" class="tsubcata">DRM</a>
+                                    <a href="#" class="tsubcata">Foxies</a>
+                                    <a href="#" class="tsubcata">Dogs</a>
+                                    <a href="#" class="tsubcata">Cats</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="bcat">
+                    <div class="title">Category two</div>
+                    <div class="bcatc">
+                        <div class="tname">
+                            <div class="bcatci">Icon</div>
+                            <div class="subd">
+                                <span>Offtop</span>
+                                <div class="tsubcat">
+                                    <a href="#" class="tsubcata">DRM</a>
+                                    <a href="#" class="tsubcata">Foxies</a>
+                                    <a href="#" class="tsubcata">Dogs</a>
+                                    <a href="#" class="tsubcata">Cats</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="bcatc">
+                        <div class="tname">
+                            <div class="bcatci">Icon</div>
+                            <div class="subd">
+                                <span>Offtop</span>
+                                <div class="tsubcat">
+                                    <a href="#" class="tsubcata">DRM</a>
+                                    <a href="#" class="tsubcata">Foxies</a>
+                                    <a href="#" class="tsubcata">Dogs</a>
+                                    <a href="#" class="tsubcata">Cats</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?=$bottom?>
