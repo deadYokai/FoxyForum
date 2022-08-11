552b7bf00fa1caffcd563ed6e822288f4d7c6ce1
OneGameKoTT <i.misterok21@gmail.com>
2022-08-11 03:12:23 +0300

    [HTML] update index template

--- index.php
+++ index.php
@@ -3,9 +3,28 @@ require("core/core.php");
 
 echo $top;
 ?>
-
+    <div class="top-wrapper">
+        <div class="title">FoxyForum</div>
+        <div class="member">
+            <div class="mblock">
+                <div class="micon">Icon</div>
+                <div class="tinfo">
+                    <div class="mname">deadYokai</div>
+                    <div class="rank">Administrator</div>
+                </div>
+                <div class="arr"><span>></span></div>
+            </div>
+        </div>
+    </div>
     <div class="content-wrapper">
         <div class="content">
+            <div class="tree">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Category</a></li>
+                    <li><a href="#">Topic</a></li>
+                </ul>
+            </div>
             <div class="cblock">
                 <div class="bcat">
                     <div class="title">Category one</div>
